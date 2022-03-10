9965e467c35562413fd02f8557ffd34f8d32496a
Nigel Hing <nigelhing1@gmail.com>
2022-03-10 18:39:21 +0800

    correct routing to heroku

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,6 +2,6 @@ export default function() {
   if( window.location.host.indexOf( 'localhost' ) !== -1 ) {    
     return 'http://localhost:3001'; 
   } else {
-    return 'https://stock-tracker-be-test.herokuapp.com/';
+    return 'https://stock-tracker-be-test.herokuapp.com'    
   }
 }
